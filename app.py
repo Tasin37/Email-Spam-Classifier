b47bdb9e620c19b4a2b3f3a176dcd80e3cf289ac
MD. Tasin Islam <109909836+Tasin37@users.noreply.github.com>
2026-04-07 22:58:26 +0600

    Refactor text transformation logic in app.py
    Refactor transform_text function to use loops for filtering and stemming.

--- app.py
+++ app.py
@@ -16,11 +16,26 @@ def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
 
-    text = [i for i in text if i.isalnum()]
-    text = [i for i in text if i not in stopwords.words('english')]
-    text = [ps.stem(i) for i in text]
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
 
-    return " ".join(text)
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+
+    return " ".join(y)
 
 # Load
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
